dfbd94b3ece7a83f5f811408fccf8d05140f6579
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-28 21:25:31 +0530

    Update SaarthiMain.jsx

--- client/src/components/SaarthiMain.jsx
+++ client/src/components/SaarthiMain.jsx
@@ -137,9 +137,9 @@ const SaarthiMain = ({ loading, prompt, setPrompt, handleAsk }) => {
       {/* Input Area */}
       <form
         onSubmit={handleAsk}
-        className="absolute bottom-0 left-0 right-0 z-10 px-2 sm:px-4 py-2 backdrop-blur-sm"
+        className="absolute bottom-0 left-0 right-0 z-10 px-2 sm:px-4 py-1"
       >
-        <div className="max-w-6xl mx-auto flex rounded-xl px-4 py-1 bg-[#f6f7f9] shadow-inner border border-gray-300">
+        <div className="max-w-8xl mx-auto flex rounded-xl px-4 py-1 bg-[#f6f7f9] shadow-inner border border-gray-300">
           <div className="relative w-full">
             <textarea
               lg:rows={3} 
