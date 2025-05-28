dd826ad28589d069f6b15fa09fb0bba9fce2666f
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-28 18:34:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -55,10 +55,7 @@ app.use("/api/gemini", geminiRouter);
 //Connect To MongoDb
 connectDB();
 
-if(process.env.NODE_ENV!=="production"){
-const PORT=process.env.PORT || 5000;
-server.listen(PORT,()=>console.log("Server is running on PORT:"+PORT))
-}
-
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
 
 export default server;
